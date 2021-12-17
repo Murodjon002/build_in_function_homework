4f4bd09614c1a3202cd60aeb41acdcec52faf72b
Jalilov <javohirjalilov777@gmail.com>
2021-12-17 23:16:58 +0500

    added tasks

---
+++ build_func01.py
@@ -0,0 +1,5 @@
+# Create a variable "number"  and assign it the value of the -8.
+
+# Find the absolute value of a "number" and assign to  "answer" 
+
+# Print the answer
---
+++ build_func07.py
@@ -0,0 +1,2 @@
+# the task condition is placed in the README file
+# https://github.com/codeschool43/Build_in_function_homework#build_func07
