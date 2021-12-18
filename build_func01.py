13611399e9a1aed838283f2df1d2304ee4bc9f8b
Murodjon <murodjondeveloper@gmail.com>
2021-12-18 07:52:50 +0500

    1

--- build_func01.py
+++ build_func01.py
@@ -1,5 +1,6 @@
 # Create a variable "number"  and assign it the value of the -8.
-
+number=-8
 # Find the absolute value of a "number" and assign to  "answer" 
-
+answer=abs(number)
 # Print the answer
+print(answer)
